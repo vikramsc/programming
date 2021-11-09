def57b6c84980bd083988f1ba0811c890899972e
Vikram Chouhan <vikramsc@users.noreply.github.com>
2021-11-09 07:12:30 +0530

    List of c++ concepts

--- c++/basics.cpp
+++ c++/basics.cpp
@@ -3,7 +3,33 @@
 using namespace std;
 
 // Basics
-// 
+// Inheritance
+// Virtual class / functions
+// Abstract class / interface, Virtual
+// templates
+// Object & Instances, constructor, copy constructor
+// Functions, inline function, function pointers, friend function, virtual/Pure Virtual functions, function overloading, function overriding
+// Call by Value & Call by Reference
+// Storage classes, goto, macros, namespaces
+// Pointers, Function Pointers, Void Pointers
+// Memory allocation, new, delete, malloc, calloc
+// Exception and Exception handling, User defined exceptions, Error handling (error.h)
+// File Stream (iostream)
+// Signal Handling in C++
+// Socket Programming
+// IPC in C++
+// Locking in C++
+// STL - components, vectors, Deque, List, Set, Stack, Queue, Priority Queue, Map, MultiMap, Bitset, Iterators
+// System programming
+
+
+/* Class
+Inheritance
+Polymorphism
+Abstraction
+Encapsulation
+ */
+
 int main() {
     cout << "First c++ program";
     return 0;
